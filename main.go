e6a14641c9e7f2d689347b0afe9bc2328e694222
mohammed jebbari <mohammed.jebbari24@ump.ac.ma>
2024-10-16 14:28:08 +0100

    push

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", groupietracker.HomeHandler)
 	http.HandleFunc("/artists", groupietracker.ArtistsHandler)
+	
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println(err)
